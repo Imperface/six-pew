75e31e96161dcda0a674f6d35ae8f7bd99a1bb22
Imperface <kovzhenko.ua@gmail.com>
2024-12-26 09:51:59 +0200

    fix deploy

---
+++ src/constants/vacancies.ts
@@ -0,0 +1,18 @@
+export const VACANCIES_LIST = [
+  {
+    name: "vacancy 1 (driver B)",
+    imgUrl: "./images/imageDriver",
+    about: "about",
+    requirements: ["req 1", "req 2"],
+    conditions: ["cond 1", "cond 2"],
+    responsibilities: ["resp 1", "resp 2"],
+  },
+  {
+    name: "vacancy 2 (driver C)",
+    imgUrl: "./images/imageDriver",
+    about: "about",
+    requirements: ["req 1", "req 2"],
+    conditions: ["cond 1", "cond 2"],
+    responsibilities: ["resp 1", "resp 2"],
+  },
+];
